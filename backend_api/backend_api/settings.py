7d943ff5224fda2be4c90ebb78ba0675d4705bf5
Leandro Pedroso <lpedroso64@outlook.com>
2023-02-12 08:50:11 -0300

    definindo endereço para o hosts

--- backend_api/backend_api/settings.py
+++ backend_api/backend_api/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-9+s&nd-@04)25)qtgkbpictu^w*-ygx6wf+wxmz6krib%7q&g2
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
