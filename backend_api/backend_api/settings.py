f31709e50f83c75153f54ade3fca53abde207b3a
Leandro Pedroso <lpedroso64@outlook.com>
2023-02-12 09:01:23 -0300

    definindo configurações para o banco de dados

--- backend_api/backend_api/settings.py
+++ backend_api/backend_api/settings.py
@@ -76,8 +76,14 @@ WSGI_APPLICATION = 'backend_api.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.bd.backends.postgresql',
+        'NAME': 'ecommerce',
+        'USER': 'postgres',
+        'PASSWORD': 'root',
+        'HOST': 'localhost',
+
     }
 }
 
